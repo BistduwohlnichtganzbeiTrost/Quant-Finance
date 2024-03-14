e03d23b91a588c14b24e15867c2ea02005a97b46
Yue Li <michaelli0914@outlook.com>
2024-03-14 00:56:23 +0000

    add FX

---
+++ Misc_Strat/FX_strat.py
@@ -0,0 +1,33 @@
+import tensorflow as tf
+
+import numpy as np
+import pandas as pd
+import random
+
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.model_selection import TimeSeriesSplit, train_test_split
+from sklearn.ensemble import RandomForestClassifier, BaggingClassifier, AdaBoostClassifier
+from sklearn.metrics import accuracy_score, f1_score, precision_score, recall_score, classification_report, confusion_matrix
+
+from keras.models import Sequential
+from keras.layers import Dense, Activation, LSTM, Dropout
+from keras.utils import to_categorical
+from keras import optimizers, metrics
+from keras import backend as K
+
+from datetime import datetime, timedelta
+
+seed = 123
+random.seed(seed)
+np.random.seed(seed)
+
+class BasicTemplateAlgorithm(QCAlgorithm):
+
+    def Initialize(self):
+
+        self.session = K.get_session()
+        self.graph = tf.get_default_graph()
+
+        self.SetStartDate(2018, 8, 1)
+        self.SetEndDate(2018, 11, 21)
+        self.SetCash(1e5)
