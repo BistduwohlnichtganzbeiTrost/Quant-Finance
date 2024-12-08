1d78376472ccf6aa21e579c7f30a435c13f4c0ae
BistduwohlnichtganzbeiTrost <michaelli0914@outlook.com>
2024-12-08 16:54:50 +0800

    add train norm_action

--- simpleDQN/model/DDPG.py
+++ simpleDQN/model/DDPG.py
@@ -1,5 +1,9 @@
 import tensorflow as tf
 from keras.layers.convolutional import Convolution2D
+from keras.layers.pooling import MaxPooling2D
+from keras.layers.core import Flatten, Lambda, Dense
+from keras.models import Sequential
+from keras.layers import BatchNormalization, Activation
 
 import numpy as np
 import pandas as pd
@@ -65,3 +69,82 @@ class DDPG(object):
             with tf.device(self.device):
                 self.build_model()
         print('finished building model!')
+
+    def train(self, input_data):
+        self.max_action = 100
+        '''
+        training DDPG, where action is confined to integer space
+
+        Args:
+            data (DataFrame): stock price for self.n_feature companies
+        '''
+         stock_data = input_data.values
+        date = input_data.index
+        T = len(stock_data)
+        
+        # frequency for output
+        print_freq = int(T / 10)
+        if print_freq == 0:
+            print_freq = 1
+            
+        print ("training....")
+        st = time.time()
+        # prioritizomg parameter
+        db = (1 - self.beta) / 1000
+        
+        # result for return value
+        values = []
+        date_label = []
+        value = 0
+        values.append(value)
+        date_label.append(date[0])
+        # keep half an year data 
+        t0 = self.n_history + self.n_batch
+        self.initialize_memory(stock_data[:t0])
+        plot_freq = 10
+        save_freq = 100000
+        count = 0
+        for t in range(t0, T - 1):
+            self.update_memory(stock_data[t], stock_data[t+1])
+            reward = self.take_action(stock_data[t], stock_data[t+1])
+            value += reward
+            date_label.append(date[t+1])
+            values.append(value)
+            count += 1
+            for epoch in range(self.n_epoch):    
+                # select transition from pool
+                self.update_weight()
+                # update prioritizing paramter untill it goes over 1
+                # self.beta  += db
+                if self.beta >= 1.0:
+                    self.beta = 1.0
+                 
+            if t % print_freq == 0:
+                print ("time:",  date[t + 1])
+                action = self.predict_action(stock_data[t+1])
+                print("portfolio:", action)
+                print("reward:", reward)
+                print("value:", value)
+                print ("elapsed time", time.time() - st)
+                print("********************************************************************")
+                
+            if count % plot_freq == 0:
+                result = pd.DataFrame(values, index=pd.DatetimeIndex(date_label))
+                result.to_csv("training_result.csv")
+                
+            if count % save_freq == 0:
+                save_path = self.saver.save(self.sess, self.save_path)
+                print("Model saved in file: %s" % self.save_path)
+
+        save_path = self.saver.save(self.sess, self.save_path)
+        print("Model saved in file: %s" % self.save_path)
+        print ("finished training")
+           
+        return pd.DataFrame(values, index=pd.DatetimeIndex(date_label))
+    
+    def norm_action(self, action):
+        max_action = np.max(np.abs(action))
+        if max_action > 1:
+            return action / max_action
+        else:
+            return action
